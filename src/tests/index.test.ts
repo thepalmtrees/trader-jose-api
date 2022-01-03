9567cdce23b7034eb305c54fc52a2a7f7192b901
thepalmtrees <96289385+thepalmtrees@users.noreply.github.com>
2022-01-03 21:42:38 +0100

    fix import

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,6 +1,6 @@
 import request from 'supertest';
 import App from '@/app';
-import IndexRoute from '@routes/index.route';
+import V1Route from '@routes/v1/v1.route';
 
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
@@ -9,8 +9,8 @@ afterAll(async () => {
 describe('Testing Index', () => {
   describe('[GET] /', () => {
     it('response statusCode 200', () => {
-      const indexRoute = new IndexRoute();
-      const app = new App([indexRoute]);
+      const route = new V1Route();
+      const app = new App([route]);
 
       return request(app.getServer()).get('/status').expect(200);
     });
